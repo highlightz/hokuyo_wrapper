0946633e3cd521217275bb8b689558e9b692f97f
Highlight Clark <highlightz@163.com>
2015-07-16 14:09:34 +0800

    Create hokuyo_wrapper.cpp

---
+++ hokuyo_wrapper.cpp
@@ -0,0 +1,102 @@
+#include "hokuyo_wrapper.h"
+
+#ifdef _DEBUG_
+ #include <iostream>
+ using std::cout;
+ using std::endl;
+#endif
+
+hokuyo_wrapper::hokuyo_wrapper( int argc, char** argv ) : information( argc, argv )
+{
+    runStatus = false;
+}
+
+bool hokuyo_wrapper::startHokuyo( )
+{
+    if ( runStatus )
+        return true;
+
+    // Open Hokuyo
+    if ( !urg.open( information.device_or_ip_name( ),
+                    information.baudrate_or_port_number( ),
+                    information.connection_type( ) ) )
+    {
+#ifdef _DEBUG_
+        cout << "Urg_driver::open( ): "
+                     << information.device_or_ip_name( )
+                     << ": " << urg.what( ) << endl;
+#endif
+        return true;
+    }
+
+    urg.set_scanning_parameter( urg.deg2step( -90 ), urg.deg2step( +90 ), 0 );
+
+    // Normally, to receive measurement data from the sensor, the function start_measurement( )
+    // and get_distance( ) are called (in pairs) many times( hand-shake measurement mode).
+    // If the number of scans is used, the function start_measurement( ) is called just once,
+    // and then only call get_distance(  ) repeatedly to receive data more effectively from the sensor.
+    urg.start_measurement( qrk::Urg_driver::Distance, qrk::Urg_driver::Infinity_times, 0 );
+
+    runStatus = true;
+
+    return true;
+}
+
+bool hokuyo_wrapper::bufferDistance( )
+{
+    if ( runStatus )
+    {
+        long time_stamp = 0;
+        if ( !urg.get_distance( distance_data, &time_stamp ) )
+        {
+#ifdef _DEBUG_
+            cout << "Urg_driver::get_distance( ): " << urg.what( ) << endl;
+#endif
+            return false;
+        }
+
+        return true;
+    }
+
+    return false;
+}
+
+vector< long > hokuyo_wrapper::getDistance( ) const
+{
+    return distance_data;
+}
+
+void hokuyo_wrapper::showDistancePoints( cv::Mat& bkground )
+{
+    bkground.create( cv::Size( 480, 320 ), CV_8UC3 );
+
+    const double maxScanDistance = urg.max_distance( );
+    const double adjustable_factor = 6;
+    const double scale = bkground.cols / 2 / maxScanDistance * adjustable_factor;
+
+    // Draw axes
+    cv::line( bkground,
+              cv::Point( 10, bkground.rows * 3 / 4 ),
+              cv::Point( bkground.cols - 10, bkground.rows * 3 / 4 ),
+              cv::Scalar( 0, 255, 0 ), 2 );
+
+    cv::line( bkground,
+              cv::Point( bkground.cols / 2, 10 ),
+              cv::Point( bkground.cols / 2, bkground.rows * 3 / 4 ),
+              cv::Scalar( 0, 255, 0 ), 2 );
+              
+    for ( int index = 0; index < distance_data.capacity( ); index++ )
+    {
+        double x = distance_data[index] * cos( urg.index2rad( index ) );
+        double y = distance_data[index] * sin( urg.index2rad( index ) );
+
+        // Coordinate transformation for convenient display
+        double tmpForSwap = x;
+        x = y;
+        y = tmpForSwap;
+        x = -x * scale + bkground.cols / 2;
+        y = -y * scale + bkground.rows * 3 / 4;
+
+        cv::circle( bkground, cv::Point( x, y ), 1, cv::Scalar( 0, 0, 255 ) );
+    }
+}
