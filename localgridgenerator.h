4308e2837de9a20c1a3ff9b2b8b8619c06486f78
Highlight Clark <highlightz@163.com>
2015-08-11 15:24:25 +0800

    Create localgridgenerator.h

---
+++ localgridgenerator.h
@@ -0,0 +1,40 @@
+#ifndef LOCALGRIDGENERATOR_H
+#define LOCALGRIDGENERATOR_H
+
+#include <vector>
+using std::vector;
+
+#define CAR_WIDTH 500
+#define CAR_LENGTH 750
+#define INTEREST_RADIUS 3881
+
+#define GRID_WIDTH 6
+#define GRID_HEIGHT 5
+
+struct grid_cell
+{
+    double centralX;
+    double centralY;
+    int occupancyValue;
+};
+
+class LocalGridGenerator
+{
+public:
+    LocalGridGenerator( );
+    void setInterestRadius( long radius );
+    void updateGrid( vector< long > distance );
+    void clearGrid( );
+    void showGrid( );
+private:
+    long interestRadius;
+    grid_cell grid[GRID_WIDTH][GRID_HEIGHT];
+
+    // Mapping: [-90 deg, +90 deg] -> [-360, +360] -> [0, 720]
+    int deg2step( double deg );
+    int step2index( int step );
+    double index2rad( int index );
+    double deg2rad( double deg );
+};
+
+#endif // LOCALGRIDGENERATOR_H
