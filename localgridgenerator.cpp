411921f0896e76991c338838496afe2a2f0f0b35
Highlight Clark <highlightz@163.com>
2015-08-15 23:05:58 +0800

    Update localgridgenerator.cpp

--- localgridgenerator.cpp
+++ localgridgenerator.cpp
@@ -65,15 +65,14 @@ void LocalGridGenerator::showGrid( )
     {
         for ( int j = 0; j < GRID_WIDTH; j++ )
         {
-            cout << setw( 11 );
+            cout << setw( 2 );
             cout << "( "
                  << grid[i][j].centralX / 1000
                  << ", "
                  << grid[i][j].centralY / 1000
                  << ", "
                  << grid[i][j].occupancyValue
-                 << " )"
-                 << '\t';
+                 << " )";
         }
         cout << endl << endl << endl << endl;
     }
@@ -99,7 +98,7 @@ void LocalGridGenerator::showGridOccupancy( cv::Mat& bkground )
                 x_display = -x_display * scale + bkground.cols / 2;
                 y_display = -y_display * scale + bkground.rows * 3 / 4;
 
-                cv::circle( bkground, cv::Point( x_display, y_display ), 5, cv::Scalar( 0, 255, 0 ), 10 );
+                cv::circle( bkground, cv::Point( x_display, y_display ), 5, cv::Scalar( 0, 0, 255 ), 10 );
 
             }
             else
@@ -112,7 +111,7 @@ void LocalGridGenerator::showGridOccupancy( cv::Mat& bkground )
                 y_display = -y_display * scale + bkground.rows * 3 / 4;
 
 
-                cv::circle( bkground, cv::Point( x_display, y_display ), 5, cv::Scalar( 0, 0, 255 ), 10 );
+                cv::circle( bkground, cv::Point( x_display, y_display ), 5, cv::Scalar( 0, 255, 0 ), 10 );
 
             }
         }
